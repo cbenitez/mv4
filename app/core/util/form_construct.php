13f1855a28452cee062d5f7e1c0e1713ee71d3c7
Christian Benitez <cbenitez@sd.com.py>
2017-08-04 18:10:08 -0400

    terminado el archivo de form control

--- app/core/util/form_construct.php
+++ app/core/util/form_construct.php
@@ -1,8 +1,10 @@
 <?php
 function input( $params ){
+    
+    $params = @json_decode( $params, true );
+
     if( count( $params ) > 0 && is_array( $params ) ):
 
-        $params = @json_decode( $params, true );
 
         switch( $params['type'] ):
             case 'password': 
@@ -49,16 +51,100 @@ function input( $params ){
                 $type = "text";
         endswitch;
 
+        if( !empty( $params['pattern'] ) ):
+            $pattern = 'pattern=".{' . $params['pattern'] . '}"';
+        endif;
+
+        if( !empty( $params['label'] ) ):
+            $label = $params['label'];
+        endif;
+
+        if( !empty( $params['name'] ) ):
+            $name = $params['name'];
+        else:
+            $name = slugit( $label );
+        endif;
+
+        if( !empty( $params['placeholder'] ) ):
+            $placeholder = 'placeholder="' . $params['placeholder'] . '"';
+        endif;
+
+        if( number( $params['maxlength'] ) > 0 ):
+            $maxlength = 'maxlength="' . $params['maxlength'] . '"';
+        endif;
+
+        if( number( $params['step'] ) > 0 ):
+            $step = 'step="' . $params['step'] . '"';
+        endif;
+
+        if( !empty( $params['max'] ) ):
+            $max = 'max="' . $params['max'] . '"';
+        endif;
+
+        if( !empty( $params['min'] ) ):
+            $min = 'min="' . $params['min'] . '"';
+        endif;
+
+        if( $params['autocomplete'] == "off" ) :
+            $autocomplete = 'autocomplete="off"';
+        endif;
+
+        if( !empty( $params['class'] ) ):
+            $class = $params['class'];
+        endif;
+
+        if( $params['required'] ):
+            $required = 'required';
+        endif;
+
+        if( $params['disabled'] ):
+            $disabled = 'disabled';
+        endif;
+
+        if( $params['readonly'] ):
+            $readonly = 'readonly';
+        endif;
+
+        if( !empty( $params['value'] ) ):
+            $value = 'value="' . $params['value'] . '"';
+        endif;
+        
+    endif;
+
+    $input = 
+    '<div class="form-group">' .
+        '<label for="' . $name . '">' . $label . '</label>' .
+        '<input type="' . $type . '" name="' . $name . '" id="' . $name . '" class="form-control ' . $class . '" ' .  $step . ' ' . $placeholder . ' ' . $required . ' ' . $readonly . ' ' . $autocomplete . ' ' . $pattern . ' ' . $disabled . ' ' . $value . ' ' . $min . ' ' . $max . ' ' . $maxlength . '>' .
+        '<div class="help-block with-errors"></div>' . 
+    '</div>';
+
+    return $input;
+}
+
+function textarea( $params ){
+
+    $params = @json_decode( $params, true );
+
+    if( count( $params ) > 0 && is_array( $params ) ):
+
         if( $params['placeholder'] ):
             $placeholder = 'placeholder="' . $params['placeholder'] . '"';
         endif;
 
-        if( $params['pattern'] ):
-            $pattern = 'pattern=".{' . $params['pattern'] . '}"';
+        if( number( $params['rows'] ) > 0 ):
+            $rows = 'rows="' . $params['rows'] . '"';
+        endif;
+
+        if( number( $params['cols'] ) > 0 ):
+            $cols = 'cols="' . $params['cols'] . '"';
         endif;
 
         if( $params['label'] ):
             $label = $params['label'];
+        endif;
+
+        if( $params['wrap'] ):
+            $wrap = $params['wrap'];
         endif;
 
         if( $params['name'] ):
@@ -79,23 +165,31 @@ function input( $params ){
             $disabled = 'disabled';
         endif;
 
-        if( $params['value'] ):
+        if( number( $params['maxlength'] ) > 0 ):
+            $maxlength = 'maxlength="' . $params['maxlength'] . '"';
+        endif;
+    
+        if( $params['readonly'] ):
+            $readonly = 'readonly';
+        endif;
+
+        if( !empty( $params['value'] ) ):
             $value = 'value="' . $params['value'] . '"';
         endif;
-        
+
     endif;
-    $input = 
-    '<div class="form-group">' .
-        '<label for="' . $name . '" class="col-sm-2 control-label">' . $label . '</label>' .
-        '<div class="col-sm-10">' .
-            '<input type="' . $type . '" name="' . $name . '" id="' . $name . '" class="form-control ' . $class . '" ' . $placeholder . ' ' . $required . ' ' . $pattern . ' ' . $disabled . ' ' . $value . '>' .
-            '<div class="help-block with-errors"></div>' . 
-        '</div>' .
+
+    $textarea = 
+    '<div class="form-group">' . 
+        '<label for="'.$name.'">'.$label.'</label>' . 
+        '<textarea class="form-control ' . $class . '" name="' . $name . '" id="' . $name . '" ' . $required . ' ' . $pattern . ' ' . $placeholder . ' ' . $wrap . ' ' . $disabled . ' ' . $cols . ' ' . $rows . ' ' . $readonly . '>' . $value . '</textarea>' . 
+        '<div class="help-block with-errors"></div>' . 
     '</div>';
-    return $input;
+
+    return $textarea;
 }
 
-function textarea( $params ){
+function select( $params ){
 
     $params = @json_decode( $params, true );
 
@@ -133,29 +227,145 @@ function textarea( $params ){
     
     endif;
 
-    $textarea = 
-    '<div class="form-group">' . 
-        '<label class="col-sm-2 control-label" for="'.$name.'">'.$label.'</label>' . 
-        '<div class="col-sm-10">' . 
-            '<textarea class="form-control ' . $class . '" name="' . $name . '" id="' . $name . '" ' . $required . ' ' . $pattern . ' ' . $placeholder . ' ' . $disabled . ' rows="7"></textarea>' . 
-            '<div class="help-block with-errors"></div>' . 
-        '</div>' . 
+    $select =
+    '<div class="form-group">' .
+        '<label for="'.$name.'">'.$label.'</label>' . 
+        '<select class="form-control ' . $class . '" name="' . $name . '" id="' . $name . '" ' . $required . ' ' . $disabled . '>' .
+            '<option value="">Seleccionar/option>' .
+            $options .
+        '</select>' .
+        '<div class="help-block with-errors"></div>' . 
     '</div>';
-    return $textarea;
-}
-
-function select( $params ){
     
+    return $select;
 }
 
 function checkbox( $params ){
+
+    $params = @json_decode( $params, true );
+
+    if( count( $params ) > 0 && is_array( $params ) ):
+
+        if( $params['label'] ):
+            $label = $params['label'];
+        endif;
+
+        if( $params['name'] ):
+            $name = $params['name'];
+        else:
+            $name = slugit( $label );
+        endif;
+
+        if( $params['class'] ):
+            $class = $params['class'];
+        endif;
+
+        if( $params['required'] ):
+            $required = 'required';
+        endif;
+
+        if( $params['disabled'] ):
+            $disabled = 'disabled';
+        endif;
     
+    endif;
+
+    $checkbox = 
+    '<div class="checkbox">' .
+        '<label>' .
+            '<input type="checkbox" name="' . $name . '" id="' . $name . '" class="form-control ' . $class . '" ' . $required . ' ' . $disabled . ' ' . $value . '>' . $label .
+        '</label>' .
+        '<div class="help-block with-errors"></div>' .
+    '</div>';
+
+    return $checkbox;
 }
 
 function radio( $params ){
     
+    $params = @json_decode( $params, true );
+
+    if( count( $params ) > 0 && is_array( $params ) ):
+
+        if( $params['label'] ):
+            $label = $params['label'];
+        endif;
+
+        if( $params['name'] ):
+            $name = $params['name'];
+        else:
+            $name = slugit( $label );
+        endif;
+
+        if( $params['class'] ):
+            $class = $params['class'];
+        endif;
+
+        if( $params['required'] ):
+            $required = 'required';
+        endif;
+
+        if( $params['disabled'] ):
+            $disabled = 'disabled';
+        endif;
+    
+    endif;
+
+    $radio = 
+    '<div class="radio">' .
+        '<label>' .
+            '<input type="radio" name="' . $name . '" id="' . $name . '" class="form-control ' . $class . '" ' . $required . ' ' . $disabled . ' ' . $value . '>' . $label .
+        '</label>' .
+        '<div class="help-block with-errors"></div>' .
+    '</div>';
+
+    return $radio;
 }
 
 function upload( $params ){
 
+    $params = @json_decode( $params, true );
+
+    if( count( $params ) > 0 && is_array( $params ) ):
+
+        if( $params['label'] ):
+            $label = $params['label'];
+        endif;
+
+        if( $params['name'] ):
+            $name = $params['name'];
+        else:
+            $name = slugit( $label );
+        endif;
+
+        if( $params['help'] ):
+            $help = $params['help'];
+        endif;
+
+        if( $params['required'] ):
+            $required = 'required';
+        endif;
+
+        if( $params['disabled'] ):
+            $disabled = 'disabled';
+        endif;
+
+        if( $params['multiple'] ):
+            $multiple = 'multiple';
+        endif;
+
+        if( $params['accept'] ):
+            $accept = 'accept="' . $params['accept'] . '"';
+        endif;
+    
+    endif;
+
+    $upload =
+    '<div class="form-group">' .
+        '<label for="' . $name . '">' . $label . '</label>' .
+        '<input type="file" name="' . $name . '" id="' . $name . '" ' . $multiple . ' ' . $required . ' ' . $disabled . ' ' . $accept . '>' .
+        '<p class="help-block">' . $help . '</p>' .
+    '</div>';
+
+    return $upload;
 }
--- app/views/public/Index.php
+++ app/views/public/Index.php
@@ -15,11 +15,22 @@ class Index extends Tova
         $page_title     = 'Mapper';
         $page_content   = 'Clase para procesamiento de pagos a través de bancard';
 
+        $params = '{"required":"true","name":"testInput","label":"Nombre","placeholder":"El nombre aqui"}';
+        $form_control = input( $params );
+        $params = '{"required":"true","name":"testInput","label":"Apellido","placeholder":"El apellido aqui"}';
+        $form_control .= input( $params );
+        $form_control .= input( $params );
+        $params = '{"required":"true","name":"testInput","label":"Texto","placeholder":"El texto aqui","rows":"7"}';
+        $form_control .= textarea( $params );
+        $params = '{"required":"true","name":"testInput","help":"La ayuda aqui."}';
+        $form_control .= upload( $params );
+
         $this->assign([
             'title_site'    => $title_site,
             'assets'        => $assets,
             'page_title'    => $page_title,
-            'page_content'  => $page_content
+            'page_content'  => $page_content,
+            'form_control'  => $form_control
         ]);
 
         $this->render();
